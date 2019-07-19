ce64834c106886c1bd4c8076ecb85a29e775e89b
aparna <aparna.chinnaiah@hipbar.com>
2019-07-19 11:53:54 +0530

    Added mobile number clickable

--- src/complaints/index.js
+++ src/complaints/index.js
@@ -137,7 +137,8 @@ class Complaints extends React.Component {
           <div className="footer">
             <div>
               <p className="text">For any other support, please contact us</p>
-              <p className="ottp-id">00 800 1008110</p>
+              {/* <p className="ottp-id">00-800-1008110</p> */}
+              <a className="ottp-id" href="tel:00-800-1008110" data-rel="external">00-800-1008110</a>
             </div>
             <div>
               <p className="text" style={{ color: "#4a90e2" }}>
